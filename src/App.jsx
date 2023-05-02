e709babf8c37bda7689f4fe37a737ab1bc1e74b0
muneneee <lukekevin39@gmail.com>
2023-05-02 15:48:31 +0300

    set up computer 3D model

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,27 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 const App = () => {
 
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-    
+    <>
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+      
 
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>  
-    </BrowserRouter>
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </div>  
+      </BrowserRouter>
+    </>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,22 +7,25 @@ import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
-        </div>
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Kevin</span></h1>
+    <>
+      <section className='relative w-full h-screen mx-auto'>
+        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
+          <div className='flex flex-col justify-center items-center mt-5'>
+            <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+            <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          </div>
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Kevin</span></h1>
 
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a software engineer that develops stunning 3D visuals, user <br className='sm:block hidden' /> interfaces and web applications 
-          </p>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              I am a software engineer that develops stunning 3D visuals, user <br className='sm:block hidden' /> interfaces and web applications 
+            </p>
+          </div>
         </div>
-      </div>
-      <ComputersCanvas />
-    </section>
+        <ComputersCanvas />
+      </section>
+    </>
+    
   )
 }
 
