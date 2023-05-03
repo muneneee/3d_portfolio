35517e36d57f3c8f908d3dc453040b163304a87d
muneneee <lukekevin39@gmail.com>
2023-05-03 17:39:22 +0300

    Added send email functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,8 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
+
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +18,45 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name]: value})
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      'service_08r7dxh',
+      'template_1iqkoak',
+      {
+        from_name: form.name,
+        to_name: 'Kevin',
+        from_email: form.email,
+        to_email: 'lukekevin39@gmail.com',
+        message: form.message,
+      },
+      'dTDOdt0Bzb_yvSiVp'
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank you, I will get back to you as soon as possible');
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+
+      console.log(error);
+
+      alert('Something went wrong')
+    })
+  };
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
